66a2bff518107c76133b01d26eed1a46052c5889
Anaveragec0der <naveragec0der@gmail.com>
2026-03-09 15:49:59 +0530

    add the terminal component intergrate the open close and drag functionality to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import gsap from 'gsap'
+import { Draggable } from 'gsap/Draggable'
+
 import {Navbar, Welcome, Dock} from '@components'
+import { Terminal } from '@windows'
 
 function App() {
-
+  gsap.registerPlugin(Draggable)
   return (
     <main>
       <Navbar/>
       <Welcome/>
       <Dock/>
+
+      <Terminal/>
     </main>
   )
 }
---
+++ src/components/WindowControls.jsx
@@ -0,0 +1,12 @@
+import useWindowStore from "@store/window"
+
+export default function WindowControls({ target }){
+    const { closeWindow } = useWindowStore()
+    return (
+        <div id="window-controls">
+            <div className="close" onClick={()=>closeWindow(target)}/>
+            <div className="minimize"/>
+            <div className="maximize" />
+        </div>
+    )
+}
